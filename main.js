8c91431d4cdb20f3fa99b1d8cb2ea9146344ff8a
John Alvin <johnalvin.bonifacio@proton.me>
2022-10-19 11:09:17 +0800

    add classlist for if else statement for switch btn

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let buttons = document.getElementsByClassName("button");
 let arr = [...buttons];
 
+const body = document.querySelector("body");
+
 arr.forEach((element, index) => {
   element.addEventListener("click", () => {
     element.style.opacity = "1";
-
+    if (index == 0) {
+      body.classList.remove("theme-01");
+      body.classList.remove("theme-02");
+    } else if (index == 1) {
+      body.classList.add("theme-01");
+      body.classList.remove("theme-02");
+    } else {
+      body.classList.add("theme-02");
+      body.classList.remove("theme-01");
+    }
     arr
       .filter(function (item) {
         return item != element;
@@ -13,11 +24,4 @@ arr.forEach((element, index) => {
         item.style.opacity = "0";
       });
   });
-});
-
-const body = document.querySelector("body");
-const title = document.querySelector(".title");
-
-title.addEventListener("click", () => {
-  body.classList.add("theme-01");
 });

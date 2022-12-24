eb473cc63b45f6f92c6205a9e779406fa78e5d8f
John Alvin <johnalvin.bonifacio@proton.me>
2022-12-24 08:15:50 +0800

    undo code for toggle changing theme

---
+++ main.js
@@ -0,0 +1,27 @@
+let buttons = document.getElementsByClassName("button");
+let arr = [...buttons];
+
+const body = document.querySelector("body");
+
+arr.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    element.style.opacity = "1";
+    if (index == 0) {
+      body.classList.remove("theme-01");
+      body.classList.remove("theme-02");
+    } else if (index == 1) {
+      body.classList.add("theme-01");
+      body.classList.remove("theme-02");
+    } else {
+      body.classList.add("theme-02");
+      body.classList.remove("theme-01");
+    }
+    arr
+      .filter(function (item) {
+        return item != element;
+      })
+      .forEach((item) => {
+        item.style.opacity = "0";
+      });
+  });
+});
